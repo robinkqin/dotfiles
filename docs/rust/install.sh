f100475b2df371adb56eafc9d41eebdf1b9ff65c
robin <robinkqin@163.com>
2020-09-30 23:25:11 +0800

    MOD: rust install

--- docs/rust/install.sh
+++ docs/rust/install.sh
@@ -4,3 +4,7 @@ export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cp config.ustc ${HOME}/.cargo/config
+
+#cargo install ripgrep fd-find exa bat
