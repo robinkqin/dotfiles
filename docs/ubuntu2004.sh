17695555a4064cb284b64fd653179994ebc7e3da
robin <robinkqin@163.com>
2020-10-04 19:51:49 +0800

    MOD: u2004

--- docs/ubuntu2004.sh
+++ docs/ubuntu2004.sh
@@ -12,30 +12,33 @@ sudo apt install -y make cmake automake autoconf build-essential gdb valgrind
 #sudo apt install -y remmina remmina-common remmina-dev remmina-plugin-exec remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice remmina-plugin-telepathy remmina-plugin-vnc remmina-plugin-xdmcp
 sudo apt install -y flameshot cloc
 #sudo apt install -y libncurses5
-#sudo apt install -y ripgrep fd-find
+sudo apt install -y ripgrep fd-find
 sudo apt install -y global silversearcher-ag ack
-#sudo apt install -y pylint pylint3
+#sudo apt install -y pylint
+sudo apt install -y pylint3
 sudo apt install -y cppcheck cppcheck-gui
 
-#sudo apt install -y python3-pip python-pip
+sudo apt install -y python3-pip
+#sudo apt install -y python-pip
 sudo apt install -y mpv ffmpeg
 sudo apt install -y audacity inkscape gimp
-#sudo apt install -y blender virtualbox shutter
+sudo apt install -y blender
+#sudo apt install -y virtualbox shutter
 sudo apt install -y samba samba-common smbclient
 
-#sudo apt install -y libavutil-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev nasm
-#sudo apt install -y libsdl2-2.0.0 libsdl2-dev libsdl2-doc libsdl2-gfx-1.0-0 libsdl2-gfx-dev libsdl2-gfx-doc libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-mixer-2.0-0 libsdl2-mixer-dev libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-ttf-2.0-0 libsdl2-ttf-dev
-#sudo apt install -y libass-dev libopenal-dev libpulse-dev libva-dev libxv-dev libegl1-mesa-dev uchardet libuchardet-dev
-#sudo apt install -y libboost-dev
+sudo apt install -y libavutil-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev nasm
+sudo apt install -y libsdl2-2.0-0 libsdl2-dev libsdl2-doc libsdl2-gfx-1.0-0 libsdl2-gfx-dev libsdl2-gfx-doc libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-mixer-2.0-0 libsdl2-mixer-dev libsdl2-net-2.0-0 libsdl2-net-dev libsdl2-ttf-2.0-0 libsdl2-ttf-dev
+sudo apt install -y libass-dev libopenal-dev libpulse-dev libva-dev libxv-dev libegl1-mesa-dev uchardet libuchardet-dev
+sudo apt install -y libboost-dev
 sudo apt install -y gperf bison flex apt-show-versions
 sudo apt install -y vainfo vdpauinfo mesa-utils
 sudo apt install -y openal-info
 
 #sudo apt install -y pavucontrol
 #sudo apt install -y virt-manager
-#sudo apt install -y wireshark
-#sudo apt install -y audacious
-#sudo apt install -y keepassxc
+sudo apt install -y wireshark
+sudo apt install -y audacious
+sudo apt install -y keepassxc
 
 #sudo apt install -y libvdpau-dev libvdpau-va-gl1 libx11-dev libv4l-dev liblzma-dev libssl-dev
 #sudo apt install -y mesa-vdpau-drivers vdpau-driver-all vdpau-va-driver
