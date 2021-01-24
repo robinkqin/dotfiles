2cc394ec8d1af127529a70d42104456dfe173bb8
robin <robinkqin@163.com>
2021-01-24 19:49:02 +0800

    MOD: debian 10

---
+++ docs/d.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt -y install vim zsh tmux git gitk ibus ibus-rime unzip htop
+apt -y install ripgrep fzf cloc ack ngrep
+apt -y install make cmake automake autoconf flex curl aria2 mpv audacity flameshot tree nasm
+#apt install vdpauinfo
+apt -y install uchardet
+apt -y install gdb valgrind
+#apt install clang lldb
+#apt install wireshark-qt
+#gpasswd -a $USER wireshark
+#apt -y install ttf-roboto noto-fonts ttf-dejavu wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+apt -y install inkscape gimp
+#apt install samba smbclient
+#apt install virtualbox
+#apt install sqlite audacious albert keepassxc
+pip -y install pygments
+pip3 -y install pygments
+#apt install pavucontrol
+
+apt -y install librime-data-wubi librime-data-pinyin-simp
+
+#apt install vulkan-headers vulkan-intel vulkan-tools vulkan-trace
+#apt install libvdpau-va-gl
+
+#yay -S wps-office ttf-wps-fonts wps-office-fonts
+
+#go rust ctags gtags bat the_silver_searcher autopep8 libva-utils qtcreator octave obs-studio
+#apt install google-chrome visual-studio-code-bin
